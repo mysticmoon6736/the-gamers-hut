240f8bebd3d4cc07150738d3500af0088036b457
mysticmoon6736 <chambersmadisonisaiah@gmail.com>
2025-02-11 14:25:24 -0500

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,2 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("The Gamer's Hut is loaded.");
-
-    // Load the navigation bar dynamically
-    fetch("/components/navbar.html")
-        .then(response => response.text())
-        .then(data => {
-            document.body.insertAdjacentHTML("afterbegin", data);
-        })
-        .catch(error => console.error("Error loading navigation:", error));
-});
